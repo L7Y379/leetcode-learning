37cc4570cee472169f6757795e2ca8d5e50e623e
L7Y379 <1102907282@qq.com>
2021-10-21 10:39:34 +0800

    剑指 Offer 31. 栈的压入、弹出序列

---
+++ leetcode/src/jianzhi/jianzhi_31.java
@@ -0,0 +1,25 @@
+package jianzhi;
+
+import java.util.Stack;
+
+public class jianzhi_31 {
+
+
+
+
+    class Solution {
+        public boolean validateStackSequences(int[] pushed, int[] popped) {
+            Stack<Integer> stack = new Stack<>();
+            int i = 0;
+            for(int num : pushed) {
+                stack.push(num); // num 入栈
+                while(!stack.isEmpty() && stack.peek() == popped[i]) { // 循环判断与出栈
+                    stack.pop();
+                    i++;
+                }
+            }
+            return stack.isEmpty();
+        }
+    }
+
+}
