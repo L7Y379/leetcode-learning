cf798c89aab742f9be123528b07ce8e1a6944450
L7Y379 <1102907282@qq.com>
2021-12-08 16:40:18 +0800

    15. 三数之和

--- leetcode/src/hot/hot_15.java
+++ leetcode/src/hot/hot_15.java
@@ -48,3 +48,41 @@ public class hot_15 {
     }
 
 }
+
+//
+//class Solution {
+//    public List<List<Integer>> threeSum(int[] nums) {
+//        List<List<Integer>> list=new ArrayList<List<Integer>>();
+//        //return list;
+//        if(nums.length<3){
+//            return list;
+//        }
+//        Arrays.sort(nums);
+//
+//        //System.out.println(toString(nums));
+//        for(int i=0;i<=nums.length-1;i++){
+//            if (i > 0 && nums[i] == nums[i - 1]) {
+//                continue;
+//            }
+//            int j=i+1;
+//            while(j<=nums.length-1){
+//                for(int k=j+1;k<=nums.length-1;k++){
+//                    if(nums[i]+nums[j]+nums[k]==0){
+//                        List<Integer> l=new ArrayList<Integer>();
+//                        l.add(nums[i]);
+//                        l.add(nums[j]);
+//                        l.add(nums[k]);
+//                        list.add(l);
+//                        break;
+//                    }
+//                }
+//                j++;
+//                while(j<=nums.length-1&&nums[j]==nums[j-1]){
+//                    j++;
+//                }
+//            }
+//
+//        }
+//        return list;
+//    }
+//}
