1cd5d34b11ed65946e349c0757ce6246abb85dcb
L7Y379 <1102907282@qq.com>
2022-03-04 11:14:29 +0800

    206. 反转链表

---
+++ leetcode/src/hot/hot_206反转链表.java
@@ -0,0 +1,46 @@
+package hot;
+
+public class hot_206反转链表 {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode reverseList(ListNode head) {
+            // if(head==null){
+            //     return null;
+            // }
+            // List<ListNode> list=new ArrayList<ListNode>();
+            // while(head!=null){
+            //     list.add(head);
+            //     head=head.next;
+            // }
+            // ListNode result=list.get(list.size()-1);
+            // ListNode p=result;
+            // for(int i=list.size()-2;i>=0;i--){
+            //     p.next=list.get(i);
+            //     p=p.next;
+            // }
+            // p.next=null;
+            // return result;
+
+            if(head==null){
+                return null;
+            }
+            ListNode p=new ListNode(head.val);
+            head=head.next;
+            while(head!=null){
+                ListNode q=new ListNode(head.val,p);
+                head=head.next;
+                p=q;
+            }
+            return p;
+        }
+    }
+}
