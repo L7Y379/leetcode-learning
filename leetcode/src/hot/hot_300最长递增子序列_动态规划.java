a3dd6465c321ee2ef793907ea5c69f98f92b616e
L7Y379 <1102907282@qq.com>
2022-03-16 14:32:25 +0800

    300. 最长递增子序列

---
+++ leetcode/src/hot/hot_300最长递增子序列_动态规划.java
@@ -0,0 +1,26 @@
+package hot;
+
+public class hot_300最长递增子序列_动态规划 {
+    class Solution {
+        public int lengthOfLIS(int[] nums) {
+            if (nums.length == 0) {
+                return 0;
+            }
+            int[] dp = new int[nums.length];
+            dp[0] = 1;
+            int maxans = 1;
+            for (int i = 1; i < nums.length; i++) {
+                dp[i] = 1;
+                for (int j = 0; j < i; j++) {
+                    if (nums[i] > nums[j]) {
+                        dp[i] = Math.max(dp[i], dp[j] + 1);
+                    }
+                }
+                maxans = Math.max(maxans, dp[i]);
+            }
+            return maxans;
+        }
+    }
+
+
+}
