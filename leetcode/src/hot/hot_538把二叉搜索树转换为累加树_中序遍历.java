c6f32c1d3b3cf1fdedd5a9c01f510599ffc8096e
L7Y379 <1102907282@qq.com>
2022-03-21 11:28:39 +0800

    538. 把二叉搜索树转换为累加树

---
+++ leetcode/src/hot/hot_538把二叉搜索树转换为累加树_中序遍历.java
@@ -0,0 +1,46 @@
+package hot;
+
+public class hot_538把二叉搜索树转换为累加树_中序遍历 {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class Solution {
+        int k;
+        public TreeNode convertBST(TreeNode root) {
+            if(root==null){
+                return null;
+            }
+            int sum=0;
+            sum=getsum(root);
+            zhongxu(root,sum);
+            return root;
+        }
+        public void zhongxu(TreeNode root,int sum){
+            if(root==null){
+                return;
+            }
+            zhongxu(root.left,sum);
+            k=k+root.val;
+            root.val=sum-k+root.val;
+            zhongxu(root.right,sum);
+        }
+        public int getsum(TreeNode root){
+            if(root==null){
+                return 0;
+            }
+            return root.val+getsum(root.left)+getsum(root.right);
+        }
+    }
+}
