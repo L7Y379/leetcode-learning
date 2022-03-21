298a184465b2d7a16bc4c49d0a2ad3bd12f31cf4
L7Y379 <1102907282@qq.com>
2022-03-21 17:06:25 +0800

    538. 把二叉搜索树转换为累加树

--- leetcode/src/hot/hot_538把二叉搜索树转换为累加树_中序遍历.java
+++ leetcode/src/hot/hot_538把二叉搜索树转换为累加树_中序遍历.java
@@ -16,31 +16,49 @@ public class hot_538把二叉搜索树转换为累加树_中序遍历 {
      *     }
      * }
      */
-    class Solution {
-        int k;
-        public TreeNode convertBST(TreeNode root) {
-            if(root==null){
-                return null;
-            }
-            int sum=0;
-            sum=getsum(root);
-            zhongxu(root,sum);
-            return root;
+//    class Solution {
+//        int k;
+//        public TreeNode convertBST(TreeNode root) {
+//            if(root==null){
+//                return null;
+//            }
+//            int sum=0;
+//            sum=getsum(root);
+//            zhongxu(root,sum);
+//            return root;
+//        }
+//        public void zhongxu(TreeNode root,int sum){
+//            if(root==null){
+//                return;
+//            }
+//            zhongxu(root.left,sum);
+//            k=k+root.val;
+//            root.val=sum-k+root.val;
+//            zhongxu(root.right,sum);
+//        }
+//        public int getsum(TreeNode root){
+//            if(root==null){
+//                return 0;
+//            }
+//            return root.val+getsum(root.left)+getsum(root.right);
+//        }
+//    }
+    int k;
+    public TreeNode convertBST(TreeNode root) {
+        if(root==null){
+            return null;
         }
-        public void zhongxu(TreeNode root,int sum){
-            if(root==null){
-                return;
-            }
-            zhongxu(root.left,sum);
-            k=k+root.val;
-            root.val=sum-k+root.val;
-            zhongxu(root.right,sum);
-        }
-        public int getsum(TreeNode root){
-            if(root==null){
-                return 0;
-            }
-            return root.val+getsum(root.left)+getsum(root.right);
-        }
+        zhongxu(root);
+        return root;
     }
+    public void zhongxu(TreeNode root){
+        if(root==null){
+            return;
+        }
+        zhongxu(root.right);
+        k=k+root.val;
+        root.val=k;
+        zhongxu(root.left);
+    }
+
 }
