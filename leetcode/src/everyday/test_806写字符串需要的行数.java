eefc12668ddbad31e68ffc92c33953a95f46cada
L7Y379 <1102907282@qq.com>
2022-04-12 14:04:03 +0800

    806. 写字符串需要的行数

--- leetcode/src/everyday/test_806写字符串需要的行数.java
+++ leetcode/src/everyday/test_806写字符串需要的行数.java
@@ -5,15 +5,23 @@ import java.util.Arrays;
 public class test_806写字符串需要的行数 {
     public static void main(String[] args) {
         int[] widths = {10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10};
-        String S = "abcdefghijklmnopqrstuvwxyz";
-        char[] S_arr=S.toCharArray();
+        String s = "abcdefghijklmnopqrstuvwxyz";
+        char[] S_arr=s.toCharArray();
         int len=0;
-        for(char s:S_arr){
-            len+=widths[s-'a'];
-        }
         int[] result=new int[2];
-        result[0]=len/100+1;
-        result[1]=len%100;
+        for(char c:S_arr){
+            int k=widths[c-'a'];
+            if(len+k<=100){
+                len+=k;
+            }else {
+                len=k;
+                result[0]++;
+            }
+
+        }
+        result[0]++;
+        result[1]=len;
+
         System.out.println(Arrays.toString(result));
     }
 }
