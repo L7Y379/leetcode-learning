3ffd67b0a487ce4525f4609e219a9bfee18c0c7f
VLU <1102907282@qq.com>
2022-06-13 09:55:10 +0800

    1051. 高度检查器

---
+++ leetcode/src/everyday/test_1051高度检查器.java
@@ -0,0 +1,20 @@
+package everyday;
+
+import java.util.Arrays;
+
+public class test_1051高度检查器 {
+    public int heightChecker(int[] heights) {
+        int[] expected=new int[heights.length];
+        for(int i=0;i<expected.length;i++){
+            expected[i]=heights[i];
+        }
+        Arrays.sort(expected);
+        int result=0;
+        for(int i=0;i<expected.length;i++){
+            if(expected[i]!=heights[i]){
+                result++;
+            }
+        }
+        return result;
+    }
+}
